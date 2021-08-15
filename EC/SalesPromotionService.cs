e8f0b28ab62fa60aa40ac260be17ea3e9564c5d9
bda605@gmail.com <bda605@gmail.com>
2021-08-15 17:44:59 +0800

    Init

---
+++ EC/SalesPromotionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EC
+{
+    public class SalesPromotionService
+    {
+        public string IsFathersDay()
+        {
+            DateTime today = DateTime.Now;
+            if (today.Month == 8 && today.Day == 8)
+            {
+                return "Is FathersDay";
+            }
+            return "Is Not FathersDay";
+        }
+    }
+}
