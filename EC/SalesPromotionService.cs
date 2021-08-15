cfc4f6d9100fa6dffeaad653277ec592209c6593
bda605@gmail.com <bda605@gmail.com>
2021-08-15 21:03:05 +0800

    DI today

--- EC/SalesPromotionService.cs
+++ EC/SalesPromotionService.cs
@@ -4,13 +4,18 @@ namespace EC
 {
     public class SalesPromotionService
     {
+        private DateTime _today;
+        public SalesPromotionService(DateTime today)
+        {
+            _today = today;
+        }
         /// <summary>
         ///練習題：電商活動 爸爸節促銷活動，檢查是否為爸爸節
         /// </summary>
         /// <returns></returns>
         public string IsFathersDay()
         {
-            DateTime today = DateTime.Now;
+            DateTime today = _today;
             if (today.Month == 8 && today.Day == 8)
             {
                 return "Is FathersDay";
--- ECTest/SalesPromotionServiceTest.cs
+++ ECTest/SalesPromotionServiceTest.cs
@@ -1,18 +1,23 @@
+using EC;
 using NUnit.Framework;
-
+using System;
 namespace ECTest
 {
     public class SalesPromotionServiceTest
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void Today_Is_FahtersDay()
         {
+            var salesPromotion = new SalesPromotionService(new DateTime(2020,8,8));
+            Assert.AreEqual("Is FathersDay", salesPromotion.IsFathersDay());
         }
 
         [Test]
-        public void Test1()
+        public void Today_IsNot_FahtersDay()
         {
-            Assert.Pass();
+            var salesPromotion = new SalesPromotionService(new DateTime(2020,8,7));
+            Assert.AreEqual("Is Not FathersDay", salesPromotion.IsFathersDay());
         }
     }
 }
